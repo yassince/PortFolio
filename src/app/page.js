806c472f1cda73886f7df1e71915445c1025d1fc
yassince <charrouferrynda.yassin@gmail.com>
2025-07-22 20:04:05 +0200

    Update page layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
-import Image from "next/image";
-import Button from "@/components/Button";
+'use client'
 import Header from "@/components/Header";
-
+import AboutMe from "@/sections/AboutMe";
+import Hero from "@/sections/Hero";
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <Hero/>
+      <AboutMe/>
     </>
   )
 }
