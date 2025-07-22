c32819b02b6c57b42dfdae73dd785d9e724d3fe9
yassince <charrouferrynda.yassin@gmail.com>
2025-07-22 20:56:48 +0200

    Fixing some bugs on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
-import Button from "./Button"
+import Link from "./Link"
 export default function Header({ }) {
     return (
         <>
-            <header className="p-6 flex flex-col md:flex-row justify-around items-baseline w-full gap-4 border border-button-header bg-gradient-to-b from-purple-400  to-button-header sticky top-0">
+            <header className="p-6 flex flex-col md:flex-row justify-around items-baseline w-full gap-4 sticky top-0">
                 <div className="logo font-bold text-4xl text-center w-full p-2 md:w-auto md:text-left md:text-6xl">
                     Yassin c.e
                 </div>
-                <nav className="navigation text-xl flex flex-col w-full md:w-auto items-center md:flex-row gap-4 ">
-                    <Button texto={'Inicio'}/>
-                    <Button texto={'Sobre Mi'}/>
-                    <Button texto={'Contactame'}/>
+                <nav className="navigation hidden md:flex text-xl flex-col w-auto items-center md:flex-row gap-4">
+                    <Link href={'#inicio'}>Inicio</Link>
+                    <Link href={'#aboutMe'}>Sobre Mi</Link>
+                    <Link href={'#expirience'}>Experiencia</Link>
+                    <Link href={'#proyects'}>Proyectos</Link>
+                    <Link href={'#contacMe'}>Contactame</Link>
                 </nav>
             </header>
         </>
