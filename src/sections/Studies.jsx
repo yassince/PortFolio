5414adc8edc1c00e62a58e3e3d0cc1a76f06baf6
yassince <charrouferrynda.yassin@gmail.com>
2025-07-24 14:41:11 +0200

    Create a CardInfo for studies and a new secction

---
+++ src/sections/Studies.jsx
@@ -0,0 +1,14 @@
+import CardInfo from "@/components/CardInfo";
+
+export default function Studies({}){
+    return(
+        <>
+            <section className="min-h-screen flex justify-center items-center flex-col ">
+                <h1 className="text-6xl font-bold">Estudios</h1>
+                <section className="studies">
+                    
+                </section>
+            </section>
+        </>
+    )
+}
