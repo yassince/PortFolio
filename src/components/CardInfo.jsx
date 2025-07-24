98ae282c0270dd81a5f013d6ad84f087e7b50856
yassince <charrouferrynda.yassin@gmail.com>
2025-07-24 14:40:19 +0200

    CardInfo for Experience

---
+++ src/components/CardInfo.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+
+export default function CardInfo({ title, children, ImageCard }) {
+    return (
+        <>
+            <section className="p-8 bg-third-purple rounded-2xl max-w-96 lg:max-w-[800px] gap-6 flex flex-col items-center overflow-hidden">
+                <div className="img lg:min-h-80 flex items-center">
+                    <Image
+                        alt="Imagen de la card"
+                        src={ImageCard}
+                        width={500}
+                        height={150}
+                        className="rounded-2xl"
+                    />
+                </div>
+                <h1 className="text-xl md:text-2xl font-bold text-center">{title}</h1>
+                <p className="md:text-xl">{children}</p>
+            </section>
+        </>
+    )
+}
--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,7 +1,25 @@
+import CardInfo from "@/components/CardInfo";
+import kyndryl from "../../public/kyndryl.webp"
+import regenerati from "../../public/pcregenerati.webp"
+
 export default function Experience({}){
     return(
         <>
-            <section>Esta es mi experiencia</section>
+            <section id="experience" className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-b from-primary-pruple to-black">
+                <h1 className="text-6xl font-bold mt-32 md:mt-0">Experiencia</h1>
+                <section className="work grid grid-cols-1 lg:grid-cols-2 gap-40 m-16">
+                    <CardInfo
+                        title={'Junior Equipo Industrialización'}
+                        ImageCard={kyndryl}
+                        children={'Formé parte de un equipo de industrialización en la que mi funcion erá la creación y prueba de artefactos mediante Ansible.\nLa prueba de los artefactos mediante AWX o en maquinas virtuales como Vagrant, VMware, Molecule, VirtualBox, etc.\nTambién creamos entronos de prueba con Kubernetes para la realización de pruebas básicas como deploy, gestion de pods, gestion de los mismos desde interfaz gráfica o desde linea de comandos, entre otras muchas más'}
+                    />
+                    <CardInfo
+                        title={'Tecnico repacación y mantenimiento de equipos informáticos'}
+                        ImageCard={regenerati}
+                        children={'En este puesto de encargaba del mantenimiento preventivo y correctivo de los equipos informaticos, tanto de parte de hardware con el software. Mis funciones erán mantenimiento de cada componenete del equipo informatico y después la instalación de SO y configuración del mismo.\nTambién me encarbada de la preparación física y lógica de los equipos.'}
+                    />
+                </section>
+            </section>
         </>
     )
 }
