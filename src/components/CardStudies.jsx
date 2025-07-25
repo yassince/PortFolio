f092799d260c482a9f737033db3a5827f9df72a4
yassince <charrouferrynda.yassin@gmail.com>
2025-07-25 14:26:57 +0200

    Create a component for studies a use it on Studies secction

--- src/components/CardStudies.jsx
+++ src/components/CardStudies.jsx
@@ -1,6 +1,12 @@
-export default function CardStudies({}){
+export default function CardStudies({title, institucion, date, children}){
     return(
         <>
+            <section className="card-studies rounded-2xl bg-secundary-purple p-6 flex flex-col gap-3 max-w-[550px]">
+                <h1 className="text-2xl font-bold md:text-4xl">{title}</h1>
+                <span className="text-xl font-semibold italic">{institucion}</span>
+                <span className="italic">{date}</span>
+                <p>{children}</p>
+            </section>
         </>
     )
 }
--- src/sections/Studies.jsx
+++ src/sections/Studies.jsx
@@ -1,12 +1,24 @@
 import CardInfo from "@/components/CardInfo";
+import CardStudies from "@/components/CardStudies";
 
-export default function Studies({}){
-    return(
+export default function Studies({ }) {
+    return (
         <>
             <section className="min-h-screen flex justify-center items-center flex-col ">
                 <h1 className="text-6xl font-bold">Estudios</h1>
-                <section className="studies">
-                    
+                <section className="studies m-4 flex flex-col lg:flex-row justify-center items-center gap-4">
+                    <CardStudies
+                        title={'Sistemas Microinformáticos y Redes'}
+                        date={'2021 - 2023'}
+                        institucion={'I.E.S Francisco Javier de Burgos'}
+                        children={'Curso enfocado en el mantenimiento y la reparación de equipos informáticos, así como en la instalación, configuración y gestión de redes locales y sistemas operativos.'}
+                    />
+                    <CardStudies
+                        title={'Desarrollo de Aplicaciones Web'}
+                        date={'2023 - 2025'}
+                        institucion={'I.E.S Francisco Javier de Burgos'}
+                        children={'Curso orientado al desarrollo de aplicaciones web, abarcando tanto el front-end como el back-end. Se trabajan lenguajes como JavaScript, Java y PHP, así como frameworks y herramientas modernas como Bootstrap, Tailwind CSS y Laravel.'}
+                    />
                 </section>
             </section>
         </>
