8752180e68a520793ccfb6bd50fd1678f449d3b2
yassince <charrouferrynda.yassin@gmail.com>
2025-07-26 12:58:52 +0200

    Refactor some visual bugs

--- src/components/CardStudies.jsx
+++ src/components/CardStudies.jsx
@@ -1,7 +1,7 @@
 export default function CardStudies({title, institucion, date, children}){
     return(
         <>
-            <section className="card-studies rounded-2xl bg-secundary-purple p-6 flex flex-col gap-3 max-w-[550px]">
+            <section className="card-studies rounded-2xl bg-secundary-purple p-6 flex flex-col gap-3 max-w-[550px] min-h-80">
                 <h1 className="text-2xl font-bold md:text-4xl">{title}</h1>
                 <span className="text-xl font-semibold italic">{institucion}</span>
                 <span className="italic">{date}</span>
