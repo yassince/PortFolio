b0329ec4cd2ab68c20169637e8c35ea84d278a35
yassince <charrouferrynda.yassin@gmail.com>
2025-07-28 13:57:01 +0200

    Remove button component

--- src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-export default function Button ({texto}){
-    const classButon = `p-2 text-center rounded-2xl bg-button-header`
-    return(
-        <>
-            <button className={classButon}>{texto}</button>
-        </>
-    )
-}
