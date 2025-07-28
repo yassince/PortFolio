112b35c1da883e832d12ef08ba5ff1e924194d8a
yassince <charrouferrynda.yassin@gmail.com>
2025-07-28 12:48:39 +0200

    Refactor Header and Hero

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -3,17 +3,18 @@ import Link from "../components/Link"
 import { useState } from "react"
 import Close from "@/svg/Close"
 import { AnimatePresence, motion } from "framer-motion"
+
 export default function Header({ }) {
     const [open, setOpen] = useState(false)
     const [animation, setAnimation] = useState('')
 
     const ChangeState = () => {
-        if(open){
+        if (open) {
             setAnimation('slide-out')
             setTimeout(() => {
                 setOpen(false)
             }, '300');
-        }else{
+        } else {
             setAnimation('slide-in')
             setOpen(true)
         }
@@ -21,16 +22,33 @@ export default function Header({ }) {
 
     return (
         <>
-            <header className="p-6 flex flex-row justify-around items-baseline w-full gap-4 md:gap-2 lg:gap-4 sticky top-0 z-20">
-                <div className="logo font-bold text-4xl text-center w-full p-2 md:w-auto md:text-left lg:text-6xl">
-                    Yassin c.e
+            <header className={`flex flex-row justify-around items-baseline w-full gap-4 md:gap-2 lg:gap-4 top-0 z-20 p-8 absolute transition-colors ease-in-out`}>
+                <div className="logo font-bold text-voodoo-50 text-4xl p-2 text-left lg:text-4xl">
+                    <a href="/">
+                        Yassin C.E
+                    </a>
                 </div>
                 <nav className="navigation hidden lg:flex text-xl flex-col w-auto items-center md:flex-row gap-4">
-                    <Link href={'#inicio'}>Inicio</Link>
-                    <Link href={'#aboutMe'}>Sobre Mi</Link>
-                    <Link href={'#experience'}>Experiencia</Link>
-                    <Link href={'#proyects'}>Proyectos</Link>
-                    <Link href={'#contacMe'}>Contactame</Link>
+                    <Link 
+                        href={'#inicio'}
+                        className={'rounded-2xl p-4 text-xl font-bold bg-primary-color-2 hover:bg-primary-color-3/15 transition-colors ease-in duration-300'}
+                    >Inicio</Link>
+                    <Link 
+                        href={'#aboutMe'}
+                        className={'rounded-2xl p-4 text-xl font-bold bg-primary-color-2 hover:bg-primary-color-3/15 transition-colors ease-in duration-300'}    
+                    >Sobre Mi</Link>
+                    <Link 
+                        href={'#experience'}
+                        className={'rounded-2xl p-4 text-xl font-bold bg-primary-color-2 hover:bg-primary-color-3/15 transition-colors ease-in duration-300'}    
+                    >Experiencia</Link>
+                    <Link 
+                        href={'#proyects'}
+                        className={'rounded-2xl p-4 text-xl font-bold bg-primary-color-2 hover:bg-primary-color-3/15 transition-colors ease-in duration-300'}
+                    >Proyectos</Link>
+                    <Link 
+                        href={'#contacMe'}
+                        className={'rounded-2xl p-4 text-xl font-bold bg-primary-color-2 hover:bg-primary-color-3/15 transition-colors ease-in duration-300'}    
+                    >Contactame</Link>
                 </nav>
                 <button onClick={ChangeState} className="transition-transform ease-in flex items-center justify-center">
                     <AnimatePresence>
@@ -43,7 +61,7 @@ export default function Header({ }) {
                                         animate={{ opacity: 1, rotate: 0 }}
                                         exit={{ opacity: 0, rotate: 90 }}
                                         transition={{ duration: .3 }}
-                                        className="absolute top-6 right-8"
+                                        className="absolute top-10 right-8"
                                     >
                                         <Close
                                             height={'50px'}
@@ -61,7 +79,7 @@ export default function Header({ }) {
                                         animate={{ opacity: 1, rotate: 0 }}
                                         exit={{ opacity: 0, rotate: -90 }}
                                         transition={{ duration: .5 }}
-                                        className="absolute top-6 right-8"
+                                        className="absolute top-10 right-8"
                                     >
                                         <Menu
                                             height={'50px'}
@@ -74,12 +92,27 @@ export default function Header({ }) {
                         }
                     </AnimatePresence>
                 </button>
-                <nav className={`${open ? 'absolute' : 'hidden'} ${animation} z-20 top-26 p-6 w-full flex flex-col gap-4 lg:hidden min-h-full items-center justify-center`}>
-                    <Link href={'#inicio'}>Inicio</Link>
-                    <Link href={'#aboutMe'}>Sobre Mi</Link>
-                    <Link href={'#experience'}>Experiencia</Link>
-                    <Link href={'#proyects'}>Proyectos</Link>
-                    <Link href={'#contacMe'}>Contactame</Link>
+                <nav className={`${open ? 'absolute' : 'hidden'} ${animation} z-20 top-30 p-6 w-full flex flex-col gap-6 lg:hidden min-h-32 items-center justify-center rounded-b-2xl bg-gradient-to-b from-5% to-45% from-transparent to-black`}>
+                    <Link 
+                        href={'#inicio'}
+                        className={'border-b-4 border-voodoo-200 text-2xl font-bold hover:scale-75 transition-transform ease-in duration-300'}
+                    >Inicio</Link>
+                    <Link 
+                        href={'#aboutMe'}
+                        className={'border-b-4 border-voodoo-200 text-2xl font-bold hover:scale-75 transition-transform ease-in duration-300'}    
+                    >Sobre Mi</Link>
+                    <Link 
+                        href={'#experience'}
+                        className={'border-b-4 border-voodoo-200 text-2xl font-bold hover:scale-75 transition-transform ease-in duration-300'}
+                    >Experiencia</Link>
+                    <Link 
+                        href={'#proyects'}
+                        className={'border-b-4 border-voodoo-200 text-2xl font-bold hover:scale-75 transition-transform ease-in duration-300'}    
+                    >Proyectos</Link>
+                    <Link 
+                        href={'#contacMe'}
+                        className={'border-b-4 border-voodoo-200 text-2xl font-bold hover:scale-75 transition-transform ease-in duration-300'}
+                    >Contactame</Link>
                 </nav>
             </header>
         </>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,13 @@
 import ArrowDown from "@/svg/ArrowDown"
 import { Typewriter } from "react-simple-typewriter"
+import Header from "./Header"
 
 export default function Hero({ }) {
     return (
         <>
-            <section id="inicio" className=" z-0 fade-in min-h-screen flex items-center justify-center flex-col gap-6 transition-all duration-500 ease-in">
-                <h1 className="font-semibold text-2xl md:text-6xl text-center min-h-16">
+            <section id="inicio" className="z-0 fade-in min-h-screen flex items-center justify-center flex-col gap-8 transition-all duration-500 ease-in bg-[url(/Hero.webp)] bg-cover bg-no-repeat">
+                <Header />
+                <h1 className="font-semibold text-2xl md:text-6xl lg:text-8xl text-center min-h-16 lg:min-h-24 text-voodoo-50">
                     <Typewriter
                         words={['Desarrollador Web FullStack', 'Apasionado por la programación']}
                         loop={true}
@@ -13,13 +15,13 @@ export default function Hero({ }) {
                         cursorBlinking="|"
                     />
                 </h1>
-                <span className="italic text-xl font-bold text-center">Apasionado por construir experiencias web intuitivas y modernas.</span>
+                <span className="italic text-xl lg:text-3xl font-bold text-center text-voodoo-50">Apasionado por construir experiencias web intuitivas y modernas.</span>
                 <a href="#aboutMe">
                     <ArrowDown
                         height={'50px'}
                         width={'50px'}
                         fill={'white'}
-                        className={'animate-bounce transition-transform ease-in z-10'}
+                        className={'animate-bounce transition-transform ease-in z-10 hover:scale-125 duration-300'}
                     ></ArrowDown>
                 </a>
             </section>
