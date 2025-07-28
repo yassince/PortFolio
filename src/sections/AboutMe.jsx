95b657314c7a05cd99269d3db7072d3dfa4ed36e
yassince <charrouferrynda.yassin@gmail.com>
2025-07-28 13:58:08 +0200

    Refactor and add styles to aboutMe secction

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -1,30 +1,40 @@
 import Profile from "../../public/Profile.webp"
 import Image from "next/image";
 import Link from "@/components/Link";
+import GitHub from "@/svg/socialNetwork/GitHub";
+import Linkedin from "@/svg/socialNetwork/Linkedin";
 
 export default function AboutMe({ }) {
     return (
         <>
-            <section id="aboutMe" className="presentation min-h-screen p-6 m-6 flex flex-col gap-10 overflow-hidden justify-center">
-                <div className="cardInfo flex flex-col lg:flex-row justify-around gap-4 items-center">
-                    <div className="Info-presentation gap-6 flex flex-col">
-                        <h1 className="text-4xl md:text-6xl min-h-16">
-                            Sobre Mi 👋
-                        </h1>
-                        <p className="max-w-4xl md:hover:tracking-wide transition-all duration-150 text-xl">
-                            Soy un desarrollador web apasionado por crear soluciones innovadoras desde cero. Me destaco por ser una persona formal, motivada, puntual y con una gran capacidad de aprendizaje. Estoy en búsqueda de oportunidades profesionales donde pueda seguir creciendo y aportar valor con mi trabajo.
-                        </p>
-                        <div className="contact flex flex-col md:flex-row justify-around max-w-2xl gap-4 text-center">
-                            <Link href={'https://github.com/yassince'}>GitHub</Link>
-                            <Link href={'https://es.linkedin.com/in/yassin-charrouf-errynda-73a208374'}>Linkedin</Link>
+            <section id="aboutMe" className="min-h-screen p-4 flex flex-col gap-6 overflow-hidden justify-center items-center bg-gradient-to-b from-15% to-100% from-primary-color-3 to-primary-color-4">
+                <h1 className="text-4xl md:text-6xl min-h-16 lg:text-8xl font-bold">
+                    Sobre Mi 👋
+                </h1>
+                <div className="presentation flex flex-col lg:flex-row justify-between items-center gap-2">
+                    <p className="text-3xl lg:text-3xl max-w-6xl lg:w-xl p-2 flex flex-col items-center gap-10">
+                        Soy un desarrollador web apasionado por crear soluciones innovadoras desde cero. Me destaco por ser una persona formal, motivada, puntual y con una gran capacidad de aprendizaje. Estoy en búsqueda de oportunidades profesionales donde pueda seguir creciendo y aportar valor con mi trabajo.
+                        <div className="contact flex flex-col md:flex-row justify-center max-w-2xl gap-8 text-center">
+                            <Link
+                                href={'https://github.com/yassince'}
+                                className={'p-4 bg-primary-color-4 rounded-2xl text-2xl hover:scale-110 transition-transform duration-300 ease-in-out'}
+                            >
+                                <GitHub width={50} height={50} fill={'white'}/>
+                            </Link>
+                            <Link
+                                className={'p-4 bg-primary-color-4 rounded-2xl text-2xl hover:scale-110 transition-transform duration-300 ease-in-out'}
+                                href={'https://es.linkedin.com/in/yassin-charrouf-errynda-73a208374'}
+                            >
+                                <Linkedin width={50} height={50} fill={'white'}/>
+                            </Link>
                         </div>
-                    </div>
-                    <div className="Image-presentation h-72 w-72 rounded-[100px] border border-button-header overflow-hidden flex items-center justify-center">
+                    </p>
+                    <div className="img_profile border-2 rounded-full overflow-hidden bg-gradient-to-b from-transparent to-black w-[200] lg:w-[400]">
                         <Image
                             src={Profile}
-                            height={350}
-                            width={300}
                             alt="Profile image"
+                            height={400}
+                            width={400}
                         />
                     </div>
                 </div>
@@ -32,3 +42,13 @@ export default function AboutMe({ }) {
         </>
     )
 }
+
+
+/* <div className="cardInfo flex flex-col justify-around gap-4 items-center">
+                    
+                    <div className="Info-presentation gap-6 flex flex-col border-2">
+                        
+                       
+                    </div>
+                </div>
+                 */
